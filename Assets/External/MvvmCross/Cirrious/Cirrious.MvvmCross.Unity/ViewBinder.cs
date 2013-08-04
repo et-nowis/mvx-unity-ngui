325fff6ac3605dd1fb61045b22e1e73f0002bf38
Jordan Marr <jnmarr AT hotmail.com>
2013-08-04 18:43:19 -0400

    - ViewBinder now takes an array of MonoBehaviour/string pairs (instead of GameObject/string pairs).  This allows user to drag NGUI game object and it should automatically detect and use the NGUI UIWidget MonoBehaviour script. - Added multiple bindings to the tip label - - Tip label changes colors as the slider moves

--- Assets/External/MvvmCross/Cirrious/Cirrious.MvvmCross.Unity/ViewBinder.cs
+++ Assets/External/MvvmCross/Cirrious/Cirrious.MvvmCross.Unity/ViewBinder.cs
@@ -6,31 +6,40 @@ using Cirrious.MvvmCross.Unity.Views;
 
 namespace AssemblyCSharp
 {
+    /// <summary>
+    /// Attach this script to the View Prefab in the Unity Editor.
+    /// </summary>
 	public class ViewBinder : MvxViewController
 	{
+        /// <summary>
+        /// Drag the GameObject that contains the NGUI control you want to bind.
+        /// Then set the binding text to one or more bindings (delimited by semicolons if more than one).
+        /// Ex: text PlayerName
+        /// </summary>
 		public ViewBinding[] Bindings = new ViewBinding[] {};
 
 		protected override void ViewDidLoad ()
 		{
-			base.ViewDidLoad ();
+			base.ViewDidLoad();
 			
 			var bindingMap = new Dictionary<object, string>();
 			foreach (var b in Bindings)
 			{
-				bindingMap.Add(b.ControlContainer, b.BindText);
+				bindingMap.Add(b.UIControl, b.BindText);
 			}
 			
 			this.AddBindings(bindingMap);
 		}
-		
 	}
 	
+    /// <summary>
+    /// Bindings class to populated via Unity Editor.
+    /// </summary>
 	[Serializable]
 	public class ViewBinding
 	{
-		public GameObject ControlContainer;
+		public MonoBehaviour UIControl;
 		public string BindText;
 	}
-	
 }
 
--- Assets/TestTutorial/ViewModels/TipViewModel.cs
+++ Assets/TestTutorial/ViewModels/TipViewModel.cs
@@ -10,11 +10,28 @@ namespace TestTutorial.ViewModels
 	[MvxUnityView("TestTutorial/Views/TipView")]
     public class TipViewModel : BaseViewModel
     {
+        public TipViewModel()
+        {
+            TipValue = .5f;
+            IsTipLabelActive = true;
+            TestSpriteName = "Light";
+
+            ShowTipLabel = 1;
+            SetTipLabelColor();
+        }
+
         private float _tipValue;
         public float TipValue
         {
             get { return _tipValue; }
-            set { _tipValue = value; RaisePropertyChanged(() => TipValue); }
+            set
+            {
+                _tipValue = value;
+                RaisePropertyChanged(() => TipValue);
+
+                ShowHideTipLabel();
+                SetTipLabelColor();
+            }
         }
 
         private float _total;
@@ -38,6 +55,13 @@ namespace TestTutorial.ViewModels
             set { _tipPercent = value; RaisePropertyChanged(() => TipPercent); Recalculate(); }
         }
 
+        private int _showTipLabel;
+        public int ShowTipLabel
+        {
+            get { return _showTipLabel; }
+            set { _showTipLabel = value; RaisePropertyChanged(() => ShowTipLabel); }
+        }
+
         private bool _isTipLabelActive;
         public bool IsTipLabelActive
         {
@@ -52,10 +76,13 @@ namespace TestTutorial.ViewModels
             set { _testSpriteName = value; RaisePropertyChanged(() => TestSpriteName); }
         }
 
-        public TipViewModel()
+        private UnityEngine.Color _tipLabelColor;
+        public UnityEngine.Color TipLabelColor
         {
+            get { return _tipLabelColor; }
+            set { _tipLabelColor = value; RaisePropertyChanged(() => TipLabelColor); }
         }
-
+        
         private void Recalculate()
         {
             TipValue = ((int)Math.Round(SubTotal * TipPercent)) / 100.0f;
@@ -83,6 +110,22 @@ namespace TestTutorial.ViewModels
                     Mvx.Trace("PressCommand");
                 });
             }
+        }
+
+        private void ShowHideTipLabel()
+        {
+            // Hide tip value label if tip value is 0
+            ShowTipLabel = TipValue > 0 ? 1 : 0;
+        }
+
+        private void SetTipLabelColor()
+        {
+            if (TipValue < .3)
+                TipLabelColor = UnityEngine.Color.red;
+            else if (TipValue < .6)
+                TipLabelColor = UnityEngine.Color.yellow;
+            else
+                TipLabelColor = UnityEngine.Color.green;
         }
     }
 }
